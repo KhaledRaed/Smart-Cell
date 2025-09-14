eece5d1b4c6e9b49ccd99ed5e91cb966ad05df61
KhaledRaed <khaledraedkhateeb@gmail.com>
2025-09-14 21:23:33 +0300

    commit

--- js/about.js
+++ js/about.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const elements = document.querySelectorAll(".fade-element");
+            const elements = document.querySelectorAll(".fade-element");
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("visible");
-                observer.unobserve(entry.target); // animate only once
-            }
+            const observer = new IntersectionObserver((entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add("visible");
+                        observer.unobserve(entry.target); // animate only once
+                    }
+                });
+            }, { threshold: 0.2 });
+
+            elements.forEach(el => observer.observe(el));
         });
-    }, { threshold: 0.2 });
-
-    elements.forEach(el => observer.observe(el));
-});
